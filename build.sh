45ff2e09a50a33f787cadcd0714661ebe03a83aa
JoWatersASC <joshua.waters365@gmail.com>
2025-04-29 11:39:26 -0400

    Added compilation of conversion program to build.sh along w/ automatic creation of output dir

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-# mkdir -p ./output
-bison -d grammar.y
-flex lexer.l
+mkdir -p ./output
+
+bison -d grammar.y &&
+flex lexer.l &&
+g++ grammar.tab.c lex.yy.c main.cpp -o output/cppygen
